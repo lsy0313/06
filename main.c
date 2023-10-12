b62f641419b9ad687ee72bce867b276882216877
lsy0313 <03lsy13@naver.com>
2023-10-12 21:26:26 +0900

    실습6

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*void square(int a) {
-     a = a * a;
-     }*/
-     
-int square(int a) {
-     return (a * a);
-     }
+// Factorial calculation function
+int factorial (int x) {
+    int i;
+    int result = 1; 
+    
+    for (i = 1; i < x + 1; i++)
+        result *= i;
+            
+    return result;
+}
+
+// Combination calculation function
+int combination(int n, int r) {
+    int div1, div2;  // numerator, denominator variables
+    
+    div1 = factorial(n);
+    div2 = factorial(n-r) * factorial(r);
+    
+    return (div1 / div2);  // combination result
+}
 
 int main(int argc, char *argv[])
 { 
-    int a = 2;
-    //square(a);
-    a = square(a);
-    printf("a = %i \n", a);
+    int n, r;
+    // Get n and r values
+    printf("input n : "); 
+    scanf("%d", &n);
+    printf("input r : ");
+    scanf("%d", &r);
+    
+    // Print combination result
+    printf("Combination result = %d\n", combination(n, r));
    
   system("PAUSE");
   
